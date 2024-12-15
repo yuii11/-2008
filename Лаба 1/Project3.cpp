fb6530fc1e682defa676b23bf56ba13c2a78d9db
yuii11 <denisgrisin83@gmail.com>
2024-12-15 17:16:06 +0300

    Add files via upload

---
+++ Лаба 1/Project3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "rus");
+    int n;
+
+    // Ввод размера массива
+    cout << "Введите размер массива: ";
+    cin >> n;
+
+    // Создание массива
+    int* arr = new int[n];  // динамическое выделение памяти
+
+    // Заполнение массива
+    cout << "Введите " << n << " целых чисел: " << endl;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    // Вывод содержимого массива
+    cout << "Содержимое массива: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Адрес начала массива: " << arr << endl;
+
+    for (int i = 0; i < n; ++i) {
+        cout << "Элемент " << i << " находится по адресу: " << &arr[i]
+            << " (расстояние от начала: " << (reinterpret_cast<char>(&arr[i]) - reinterpret_cast<char>(arr)) << " байт)"
+            << endl;
+    }
+    delete[] arr;
+
+    return 0;
+}
